b130a3cec08befe6c8fc92900b84f86391352606
Alec <alhampton9@gmail.com>
2026-04-01 22:32:14 -0600

    Added bounds checking and helping methods to map

--- src/map.rs
+++ src/map.rs
@@ -37,4 +37,22 @@ impl Map {
             }
         }
     }
+
+    pub fn is_in_bounds(&self, point: Point) -> bool {
+        point.x >= 0 && point.x < SCREEN_WIDTH
+            && point.y >= 0 && point.y < SCREEN_HEIGHT
+    }
+
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        self.is_in_bounds(point)
+            && self.tiles[map_idx(point.x, point.y)] == TileType::Floor
+    }
+
+    pub fn try_idx(&self, point: Point) -> Option<usize> {
+        if !self.is_in_bounds(point) {
+            None
+        } else {
+            Some(map_idx(point.x, point.y))
+        }
+    }
 }
