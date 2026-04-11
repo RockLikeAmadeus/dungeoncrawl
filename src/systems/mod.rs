8f6d95c6b5a4d14b81c58c5ea2256432352dd5f7
Alec <alhampton9@gmail.com>
2026-04-10 21:43:05 -0600

    Added random movement for monsters

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,7 +1,10 @@
+use crate::prelude::*;
+
 mod entity_render;
 mod map_render;
 mod player_input;
 mod collision;
+mod random_move;
 
 use crate::prelude::*;
 
@@ -9,7 +12,9 @@ pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
         .add_system(collision::collisions_system())
+        .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(random_move::random_move_system())
         .build()
 }
